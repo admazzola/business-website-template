81110fcecf0f3f10d2cf30d86d669bbfb4bccc6b
andy <admazzola@gmail.com>
2022-01-10 15:03:08 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app" class=" ">
+    <router-view class="flex-1" style=" "/>
+
+  </div>
+</template>
+ 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,60 @@
+<template>
+
+<div>
+
+   <div class="section  bg-white border-b-2 border-black px-0 lg:px-1">
+
+     <div class=" ">
+        <Navbar 
+        v-bind:web3Plug="web3Plug"
+       />
+     </div>
+
+
+   </div>
+
+   
+
+
+      <div class=" section  mb-4">
+        <div class="autospacing w-container">
+          <div> File not found! </div>
+          <router-link to="/"> -> Go Home</router-link>
+
+          </div>
+      </div>
+
+  <Footer/>
+
+    
+
+ 
+
+</div>
+</template>
+
+
+<script>
+
+  
+
+
+
+
+export default {
+  name: 'NotFound',
+  props: [],
+  components: { },
+  data() {
+    return {
+       
+    }
+  },
+  created: function () {
+   
+  }, 
+  methods: {
+      
+  }
+}
+</script>
